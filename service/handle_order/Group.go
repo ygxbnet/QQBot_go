5b040f6b99a3cdb2f641df19807bc3987f5b8e3c
YGXB-net <ygxb-net@outlook.com>
2022-10-23 10:13:47 +0800

    版本: 0.7.6

--- service/handle_order/Group.go
+++ service/handle_order/Group.go
@@ -16,7 +16,8 @@ import (
 var help_info = "----------帮助信息----------" +
 	"\n\n/help 获取帮助" +
 	"\n/info 获取机器人信息" +
-	"\n\n/dk 进行打卡"
+	"\n\n/dk 进行打卡" +
+	"\n/sp 进行刷屏"
 
 var info = "本机器人由YGXB_net开发" +
 	"\nQQ:3040809965" +
