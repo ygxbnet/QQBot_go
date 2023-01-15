ea0a28d832e2f6aaa609b92990a0242c108ea767
YGXB_net <ygxb-net@outlook.com>
2023-01-15 14:30:51 +0800

    增加及修改定时任务

--- service/timerEvent.go
+++ service/timerEvent.go
@@ -5,57 +5,58 @@ import (
 	"QQBot_go/internal/httpapi"
 	"QQBot_go/service/services_api"
 	"fmt"
-	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
+	"io"
+	"net/http"
 	"strconv"
 	"time"
 )
 
 // Init 初始化
 func Init() {
-	var count = 0
-
-	// 每10min定时向Test群发送消息
+	// 每10min定时向 Test 群发送消息
 	go func() {
+		var count = 0
 		for true {
 			count = count + 1
 			httpapi.SendGroupMsg(config.Parse().Group.InfoID, "每10min定时发送\n次数: "+strconv.Itoa(count))
-			// time.Sleep(time.Second * 10)
 			time.Sleep(time.Minute * 10)
 		}
 	}()
 
-	// 每天8点定时发送Bing的每日壁纸
+	// 0点定时发送相关消息
 	go func() {
 		for {
-			log.Info("每日获取Bing壁纸开启")
-			// 开启计时
-			t := time.NewTimer(getTimeDifference(6, 0, 0))
+			t := time.NewTimer(getTimeDifference(0, 0, 0))
 			<-t.C
+
+			// 发送消息
+			timeMessage := fmt.Sprintf("今天是: %d年%d月%d日 星期%s", time.Now().Year(), time.Now().Month(), time.Now().Day(), conversionWeek(time.Now().Weekday().String()))
+			httpapi.SendGroupMsg(config.Parse().Group.MainID, timeMessage)
 
 			urlImg, nameImg := services_api.GetBingPictureURL()
 			httpapi.SendGroupMsg(config.Parse().Group.MainID, "[CQ:image,file="+nameImg+",subType=0,url="+urlImg+"]")
-
-			timeMessage := fmt.Sprintf("今天是: %d年%d月%d日 星期%s", time.Now().Year(), time.Now().Month(), time.Now().Day(), conversionWeek(time.Now().Weekday().String()))
-			httpapi.SendGroupMsg(config.Parse().Group.MainID, timeMessage)
 		}
 	}()
 
-	// 每天晚上9点提醒未打卡的打卡
-	// go func() {
-	//	for {
-	//		t := time.NewTimer(getTimeDifference(20, 0, 0))
-	//		<-t.C
-	//
-	//		now := time.Now()
-	//		August31 := time.Date(2022, time.August, 31, 0, 0, 0, 0, time.Local)
-	//		if now.Sub(August31) < 0 {
-	//			httpapi.SendGroupMsg("1038122549", "[CQ:at,qq=all] 快点来打卡呀！")
-	//		} else if now.Sub(August31) < time.Hour*24 {
-	//			httpapi.SendGroupMsg("1038122549", "[CQ:at,qq=all]\n现在是假期的最后一天了，也是假期最后一天打卡了，我破例一回，就当作每一天都打卡了。")
-	//			httpapi.SendGroupMsg("1038122549", "快点来打卡吧！")
-	//		}
-	//	}
-	// }()
+	// 6点定时发送问好
+	go func() {
+		for {
+			t := time.NewTimer(getTimeDifference(6, 0, 0))
+			<-t.C
+
+			response, _ := http.Get("https://v.api.aa1.cn/api/time-tx/index.php")
+			if response.StatusCode == 200 {
+				bytes, _ := io.ReadAll(response.Body)
+				msg := gjson.Parse(string(bytes))
+
+				message := fmt.Sprintf("%s\n%s", msg.Get("msg").String(), msg.Get("nxyj").String())
+				httpapi.SendGroupMsg(config.Parse().Group.MainID, message)
+			} else {
+				httpapi.SendGroupMsg(config.Parse().Group.MainID, "早上好！！！")
+			}
+		}
+	}()
 }
 
 // getTimeDifference 获取时间差
