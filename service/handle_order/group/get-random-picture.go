8b8b8e701ba3594f86e610e524136c38ab7f93ea
YGXB_net <ygxb-net@outlook.com>
2023-01-15 12:35:08 +0800

    更新获取随机图片地址

--- service/handle_order/group/get-random-picture.go
+++ service/handle_order/group/get-random-picture.go
@@ -4,14 +4,13 @@ import (
 	"QQBot_go/internal/httpapi"
 	"fmt"
 	log "github.com/sirupsen/logrus"
-	"math/rand"
 	"strconv"
 	"strings"
 )
 
 func GetRandomPicture(groupID string, userID string, message string) {
 	if len(strings.Fields(message)) == 1 {
-		httpapi.SendGroupMsg(groupID, "[CQ:image,file=https://api.ixiaowai.cn/gqapi/gqapi.php?a="+strconv.Itoa(rand.Int())+"]")
+		httpapi.SendGroupMsg(groupID, "[CQ:image,file=https://v.api.aa1.cn/api/api-fj-1/index.php?aa1=yuantu,cache=0]")
 	} else if len(strings.Fields(message)) == 2 {
 		num, err := strconv.Atoi(strings.Fields(message)[1])
 		if err != nil {
@@ -19,8 +18,8 @@ func GetRandomPicture(groupID string, userID string, message string) {
 			httpapi.SendGroupMsg(groupID, fmt.Sprintf("[CQ:at,qq=%s]"+"\n❌指定图片数量错误", userID))
 			return
 		}
-		for i := 0; i < num && i < 10; i++ {
-			httpapi.SendGroupMsg(groupID, "[CQ:image,file=https://api.ixiaowai.cn/gqapi/gqapi.php?a="+strconv.Itoa(rand.Int())+"]")
+		for i := 0; i < num && i < 5; i++ {
+			httpapi.SendGroupMsg(groupID, "[CQ:image,file=https://v.api.aa1.cn/api/api-fj-1/index.php?aa1=yuantu,cache=0]")
 		}
 	} else {
 		httpapi.SendGroupMsg(groupID, "❌参数错误或多余")
