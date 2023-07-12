5a72a65410e501bf169de084e4743e6a02e28ce0
YGXB_net <ygxb-net@outlook.com>
2023-07-12 18:09:39 +0800

    增加 OpenAI API 设置

--- internal/config/config-model.go
+++ internal/config/config-model.go
@@ -5,14 +5,22 @@ type Config struct {
 	Account        Account `yaml:"account"`
 	Group          Group   `yaml:"group"`
 	Server         Server  `yaml:"server"`
+	OpenAI         OpenAI  `yaml:"openai"`
 }
+
 type Account struct {
 	BotID   string `yaml:"bot-id"`
 	AdminID string `yaml:"admin-id"`
 }
+
 type Group struct {
 	MainID string `yaml:"main-id"`
 	InfoID string `yaml:"info-id"`
+}
+
+type Server struct {
+	HTTPAPI   HTTPAPI   `yaml:"http-api"`
+	Websocket Websocket `yaml:"websocket"`
 }
 type Websocket struct {
 	URL string `yaml:"url"`
@@ -20,7 +28,8 @@ type Websocket struct {
 type HTTPAPI struct {
 	URL string `yaml:"url"`
 }
-type Server struct {
-	HTTPAPI   HTTPAPI   `yaml:"http-api"`
-	Websocket Websocket `yaml:"websocket"`
+
+type OpenAI struct {
+	BaseURL string `yaml:"base-url"`
+	APIKey  string `yaml:"api-key"`
 }
