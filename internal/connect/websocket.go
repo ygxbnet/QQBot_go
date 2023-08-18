ef3966e67c36768d8730208b93b7414fc8cb0272
YGXB_net <ygxb-net@outlook.com>
2023-08-18 22:58:38 +0800

    Update websocket.go

--- internal/connect/websocket.go
+++ internal/connect/websocket.go
@@ -13,38 +13,38 @@ import (
 func Connect() {
 	webSocketURL := config.Get().Server.Websocket.URL
 
-	log.Info("正在连接: ", webSocketURL)
 	// 连接WebSocket
+	log.Info("正在连接 go-cqhttp: ", webSocketURL)
 	c, _, err := websocket.DefaultDialer.Dial(webSocketURL, nil)
 	if err != nil {
-		log.Error("连接错误: ", err)
-		time.Sleep(time.Second * 4)
-		log.Info("正在重连......")
-		time.Sleep(time.Second * 4)
+		log.Error("连接失败，正在重连: ", err)
 
 		if c != nil {
 			c.Close()
 		}
-		Connect()
-	} else {
-		log.Info(webSocketURL + " 连接成功")
-		// 接受消息
-		go func() {
-			for {
-				_, message, err := c.ReadMessage()
-				if err != nil {
-					log.Error("消息接受错误: ", err)
 
-					if c != nil {
-						c.Close()
-					}
-					log.Info("正在重连")
-					break
-				} else {
-					handler.EventHandler(string(message)) // 处理消息
-				}
-			}
-			Connect()
-		}()
+		time.Sleep(time.Second * 5)
+		Connect()
+		return
 	}
+
+	log.Info(webSocketURL + " 连接成功")
+	// 接受消息
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Error("消息接受错误: ", err)
+
+				if c != nil {
+					c.Close()
+				}
+				log.Info("正在重连")
+				break
+			} else {
+				handler.EventHandler(string(message)) // 处理消息
+			}
+		}
+		Connect()
+	}()
 }
